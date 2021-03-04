fce6c4e46046a30f356cf92258105d6c51583df7
meniAvitan <66748984+meniAvitan@users.noreply.github.com>
2021-03-04 10:07:13 +0200

    fixed and old ref db

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/imageurl' , (req,res) => { handleApiCall(req, res)});
 
 
 
-app.listen(3000, ()=>{
-    console.log('app is running at port 3000');
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+  });
 
